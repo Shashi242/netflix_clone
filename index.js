4975083647ea46b418b2219f1adbdfe3a9d89536
Shashi Raj <raj34828@gmail.com>
2023-05-19 16:49:15 +0530

    my first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const movieRoute = require("./routes/movies");
+const listRoute = require("./routes/lists");
+const cors = require("cors");
+const path = require("path");
+
+const PORT = process.env.PORT || 5000;
+
+dotenv.config();
+app.use(express.json());
+app.use(cors());
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    // useFindAndModify: false,
+    useUnifiedTopology: true,
+    // useCreateIndex: true,
+  })
+  .then(() => console.log("DB Connection Successfull"))
+  .catch((err) => {
+    console.error(err+"Connection failed");
+  });
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/movies", movieRoute);
+app.use("/api/lists", listRoute);
+
+// if(process.env.NODE_ENV == "production"){
+//   app.use(express.static("client/build"));
+// }
+
+app.use(express.static(path.join(__dirname + "/public")));
+
+app.get("*", function (req, res){
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+})
+
+app.listen(PORT, () => {
+  console.log("Backend server is running!");
+});
