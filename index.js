902784a5b7f13151fdf4271a88aba5c168bbaedd
Shashi Raj <raj34828@gmail.com>
2023-05-19 17:48:02 +0530

    changes

--- index.js
+++ index.js
@@ -36,10 +36,10 @@ app.use("/api/lists", listRoute);
 //   app.use(express.static("client/build"));
 // }
 
-app.use(express.static(path.join(__dirname + "/client")));
+app.use(express.static(path.join(__dirname , "./client/build")));
 
 app.get("*", function (req, res){
-  res.sendFile(path.join(__dirname, "/client/build"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 })
 
 app.listen(PORT, () => {
