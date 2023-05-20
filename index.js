85f3cb89a3527783bfbd23e2561f0d06e8b1db12
Shashi Raj <raj34828@gmail.com>
2023-05-21 02:39:39 +0530

    changes

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const listRoute = require("./routes/lists");
 const cors = require("cors");
 const path = require("path");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8800;
 
 dotenv.config();
 app.use(express.json());
