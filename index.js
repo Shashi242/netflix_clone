47b5685de547ff8a910ffe5be7889d2bbe48293c
Shashi Raj <84275426+Shashi242@users.noreply.github.com>
2023-05-25 21:15:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,11 +36,11 @@ app.use("/api/lists", listRoute);
 //   app.use(express.static("client/build"));
 // }
 
-app.use(express.static(path.join(__dirname , "./client/build")));
+// app.use(express.static(path.join(__dirname , "./client/build")));
 
-app.get("*", function (req, res){
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-})
+// app.get("*", function (req, res){
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// })
 
 app.listen(PORT, () => {
   console.log("Backend server is running!");
